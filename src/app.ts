ff188185951766bcd2c390c94d0124ff11d4914a
Pedro Faustino <faustinopedro.faustino@gmail.com>
2021-05-03 12:26:29 -0300

    added morgan and helmet to package.json and app.ts; added files to src folders to make them appear in the repo

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import { routes } from "./routes";
 
 const app = express();
 
-app.use(routes)
+app.use(routes);
+app.use(morgan('dev'));
+app.use(helmet());
 
 export { app };
